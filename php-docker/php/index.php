eb0d8b8d9de8cf2e29a10949d6d819d63ac9f9a7
BKIslem <benkhaled.islem3@gmail.com>
2023-05-12 15:49:29 +0200

    start-commit

---
+++ php-docker/php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+include "./includes/head.inc.html"
+?>
+<body> 
+<?php
+include "./includes/header.inc.html";
+?>
+      <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-3">
+        </div>
+        <div class="col-md-9">
+         
+            <button type="button" class="btn btn-primary">Ajouter des données</button>
+        </div>
+      </div>
+        </div>
+
+</body>
+</html>
